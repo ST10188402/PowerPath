facedf247414838abd9b4134be4d2b3a2790c2ac
EB <st10188402@vcconnect.edu.za>
2024-09-26 02:19:44 +0200

    spent 8000 years tryna get the bottom nav to not float from the bottom and just ended up removing it. tomorrow is another day

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    implementation("androidx.cardview:cardview:1.0.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- app/src/main/java/com/opsc/powerpath/MainActivity.kt
+++ app/src/main/java/com/opsc/powerpath/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.opsc.powerpath.Fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        navView = findViewById(R.id.nav_view)
+
+
+        navView.setOnItemSelectedListener(){
+            when(it.itemId){
+                R.id.blank -> replaceFragment(HomeFragment())
+                R.id.home -> replaceFragment(HomeFragment())
+
+            }
+            true
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
+    }
+
+    private fun replaceFragment(fragment: androidx.fragment.app.Fragment) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_host, fragment)
+        transaction.commit()
     }
 }
