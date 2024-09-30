be165f39a8351fdd4a2ed8f2f569be57199414ff
ST10158643 <ST10158643@vcconnect.edu.za>
2024-09-30 18:23:57 +0200

    Working on the fab

--- app/src/main/java/com/opsc/powerpath/HomeFragment.kt
+++ app/src/main/java/com/opsc/powerpath/HomeFragment.kt
@@ -1,7 +1,40 @@
 package com.opsc.powerpath
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
 
+class HomeFragment : Fragment() {
+    private lateinit var userName: TextView
+    private lateinit var btnNot: ImageView
+
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,
+
+        savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        userName = view.findViewById(R.id.welcome_msg)
+        userName.text =  CurrentUser.name
+
+        btnNot = view.findViewById(R.id.btnNot)
+        btnNot.setOnClickListener {
+          //  val intent = Intent(activity, NotificationsActivity::class.java)
+           // startActivity(intent)
+        }
+
+        // Add BMIFragment to the HomeFragment
+        val transaction: FragmentTransaction = childFragmentManager.beginTransaction()
+        transaction.replace(R.id.bmi, BMIFragment())
+        transaction.commit()
+
+        return view
+    }
+}
