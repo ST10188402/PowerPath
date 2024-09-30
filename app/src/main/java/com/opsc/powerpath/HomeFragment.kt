eab9776fc58d727948eb1dd699faa69051242501
EB <st10188402@vcconnect.edu.za>
2024-09-30 22:38:10 +0200

    api calls work

--- app/src/main/java/com/opsc/powerpath/HomeFragment.kt
+++ app/src/main/java/com/opsc/powerpath/HomeFragment.kt
@@ -10,23 +10,29 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
-import com.google.firebase.Firebase
-import com.google.firebase.database.database
+import com.google.firebase.auth.FirebaseAuth
+import com.opsc.powerpath.Data.Models.User
+import com.opsc.powerpath.Utils.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeFragment : Fragment() {
     private lateinit var userName: TextView
     private lateinit var btnNot: ImageView
 
-    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,
-
-        savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         userName = view.findViewById(R.id.welcome_msg)
+        getUserDetails()
 
         btnNot = view.findViewById(R.id.btnNot)
         btnNot.setOnClickListener {
-          //  val intent = Intent(activity, NotificationsActivity::class.java)
-            //startActivity(intent)
+            val intent = Intent(activity, NotificationsActivity::class.java)
+            startActivity(intent)
         }
 
         // Add BMIFragment to the HomeFragment
@@ -35,5 +41,26 @@ class HomeFragment : Fragment() {
         transaction.commit()
 
         return view
+    }
+
+    private fun getUserDetails() {
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+        val apiService = RetrofitInstance.api.getUserById(userId)
+        apiService.enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    val user = response.body()
+                    user?.let {
+                        userName.text = "${it.name} ${it.surname}"
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Failed to get user details: ${response.message()}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Toast.makeText(requireContext(), "API call failed: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
