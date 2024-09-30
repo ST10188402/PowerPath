eae82e5258b14b6484896e8e2d30c1b91952277f
KhoiSan69420 <stoltz206@gmail.com>
2024-09-30 23:05:00 +0200

    keegan then goat

--- app/src/main/java/com/opsc/powerpath/ProgressPage.kt
+++ app/src/main/java/com/opsc/powerpath/ProgressPage.kt
@@ -1,10 +1,12 @@
 package com.opsc.powerpath
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -58,6 +60,12 @@ class ProgressPage : Fragment() {
             GalleryItem(0, "5 May")
         )
         adapter.addGalleryItems(initialItems)
+
+        val compp = view.findViewById<Button>(R.id.comparebtn)
+        compp.setOnClickListener(){
+            val intent = Intent(requireContext(), ComparisonFragment::class.java)
+            startActivity(intent)
+        }
     }
 
     companion object {
