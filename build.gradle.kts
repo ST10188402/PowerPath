811ccc4c72328203dfd11b37d347392ff17cfa44
KhoiSan69420 <stoltz206@gmail.com>
2024-11-02 15:47:11 +0200

    Reset passw functionality

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.0.2")
+        classpath ("com.android.tools.build:gradle:8.7.2")
         classpath  ("com.google.gms:google-services:4.4.2")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 
     }
 }
