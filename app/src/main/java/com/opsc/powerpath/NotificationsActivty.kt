b35d2f644e5032b7bbff118240f253f2565ab5d3
ST10158643 <ST10158643@vcconnect.edu.za>
2024-11-03 19:49:49 +0200

    Update to UI 2

--- app/src/main/java/com/opsc/powerpath/NotificationsActivty.kt
+++ app/src/main/java/com/opsc/powerpath/NotificationsActivty.kt
@@ -1,9 +1,14 @@
 package com.opsc.powerpath
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.firebase.auth.FirebaseAuth
 
 class NotificationsActivity : AppCompatActivity() {
 
@@ -22,9 +27,37 @@ class NotificationsActivity : AppCompatActivity() {
             Notification("Don’t miss your lowerbody workout", "About 3 hours ago", R.drawable.ic_notiwork),
             Notification("Oopsie, You have missed your workout", "3 April", R.drawable.ic_notiwork)
         )
+        val toolbar: MaterialToolbar = findViewById(R.id.top)
+        setSupportActionBar(toolbar)
 
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
         // Set up the adapter
         notificationAdapter = NotificationAdapter(notifications)
         recyclerView.adapter = notificationAdapter
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_nav, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu -> {
+                true
+            }
+
+            R.id.logout -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
